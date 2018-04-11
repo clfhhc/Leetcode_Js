5fe0bbe2eb8427d9cfa5c40b8607dc3cf2703f20
clfhhc <clfhhc@gmail.com>
2018-04-10 23:05:44 -0700

    9. Palindrome Number

---
+++ 9. Palindrome Number.js
@@ -0,0 +1,29 @@
+/*
+9. Palindrome Number
+
+Determine whether an integer is a palindrome. Do this without extra space.
+*/
+
+
+//Solution:
+/**
+ * @param {number} x
+ * @return {boolean}
+ */
+var isPalindrome = function(x) {
+    x=x.toString();
+    for (let i=0;i<x.length;i++) {
+        if (x.charAt(i)!==x.charAt(x.length-i-1)) {
+            return false;
+        }
+    }
+    return true;
+};
+
+
+//Test:
+const chai = require('./Chai.js/chai');
+
+chai.expect(isPalindrome(123321)).to.equal(true);
+chai.expect(isPalindrome(123)).to.equal(false);
+chai.expect(isPalindrome(856658)).to.equal(true);
