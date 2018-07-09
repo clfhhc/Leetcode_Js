b9c1d1422fe279c1e19fb97e5df2aba348ac1412
clfhhc <clfhhc@gmail.com>
2018-07-09 00:18:23 -0700

    bitwise solution to 200. Number of Islands.js

--- 200. Number of Islands.js
+++ 200. Number of Islands.js
@@ -28,68 +28,52 @@ Output: 3
  * @return {number}
  */
 const numIslands = function(grid) {
-  if (!grid.length) return 0;
-  let row0, island0, counter;
-  let row1 = 0;
-  let island1 = [];
-  let islands = [];
-  const findRoot = (array, index) => {
-    while (array[index] !== index) {
-      index === array[index];
-    }
-    return index;
-  }
-  const union = (array, index1, index2) => {
-    const root1 = findRoot(array, index1);
-    const root2 = findRoot(array, index2);
-    if (root1 === root2) { return; }
-    let temp;
-    if (root1 < root2) {
-      while (array[index1] !== array[index2]) {
-        temp = array[index2]
-        array[index2] = root1;
-        index2 = temp;
-      }
-      return;
-    }
-    while (array[index1] !== array[index2]) {
-      temp = array[index1]
-      array[index1] = root2;
-      index1 = temp;
-    }
-  }
-  grid.forEach((row) => {
-    row0 = row1;
-    row1 = parseInt(row.join(''), 2);
-    row0 = row0 & row1;
-    island0 = island1.slice();
-    island1 = [];
-    counter = 0;
-    while (row1 >> counter) {
-      if (row1 & (1 << counter)) {
-        if (row0 & (1 << counter)) { island1[counter] = island0[counter]; }
-        // island1[counter] = island1[counter] === undefined ? island1[counter - 1] : island1[counter];
-        if (!(island1[counter - 1] === undefined)) {
-          // if (island1[counter] !== island1[counter - 1]) {
-          //   if (findRoot(island, island1[counter]) !== findRoot(island, island[counter - 1])) {
-              
-          //   }
-          // }
-          if (island1[counter] === undefined) {
-            island1[counter] = island1[counter - 1];
-          } else {
-            union(islands, island1[counter], island1[counter - 1]);
-          }
-        } else if (island1[counter] === undefined) {
-          islands[islands.length] = island1[counter] = islands.length;
-        }
-      }
-      counter ++;
-    }
+  if (!grid.length) { return 0; }
+  const gridBin = [];
+  grid.forEach(row => {
+    gridBin.push(parseInt(row.join(''), 2));
   })
-  islandObj = {};
-  islands.forEach((_, index) => { islandObj[findRoot(islands,index)] = true });
-  return Object.keys(islandObj).length;
+  let islandCounter = 0;
+  let counter = 0
+  const recurse = (i, j) => {
+    console.log(i, j.toString(2));
+    gridBin[i] ^= j;
+    let nextI, nextJ;
+
+    for (let k = 0; k < 4; k++) {
+      switch (k) {
+        case 0:
+          nextI = i - 1;
+          nextJ = j;
+          break;
+        case 1:
+          nextI = i;
+          nextJ = j >> 1;
+          break;
+        case 2:
+          nextI = i + 1;
+          nextJ = j;
+          break;
+        case 3:
+          nextI = i;
+          nextJ = j << 1;
+      }
+      if (nextI >= 0 && nextI < grid.length && (gridBin[nextI] & nextJ)) {
+        recurse(nextI, nextJ);
+      }
+    }
+
+  }
+
+  let row = 0;
+  while (row < gridBin.length) {
+    while (gridBin[row]) {
+      islandCounter++;
+      recurse(row, -gridBin[row] & gridBin[row]);
+    }
+    row++;
+  }
+  return islandCounter;
 };
 
 
@@ -100,11 +84,10 @@ const chai = require('./Chai.js/chai');
 let inputs = [
   [["1","1","1","1","0"],["1","1","0","1","0"],["1","1","0","0","0"],["0","0","0","0","0"]],
   [["1","1","0","0","0"],["1","1","0","0","0"],["0","0","1","0","0"],["0","0","0","1","1"]],
+  [["1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","0","0","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","0","0","0","0","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1"]]
 ];
-let expecteds = [1, 3];
-inputs.forEach((input, index) => {
-  console.log('pause');
-  chai.expect(numIslands(input)).to.equal(expecteds[index]);
-})
-
-// numIslands(input[0])
+let expecteds = [1, 3, 3];
+// inputs.forEach((input, index) => {
+//   chai.expect(numIslands(input)).to.equal(expecteds[index]);
+// })
+numIslands(inputs[2]);
